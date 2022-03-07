4a3b999c086b243bf533c1c53f978dc7460e80ea
QtKai <dylanguy2005@gmail.com>
2022-03-07 12:11:25 +0000

    Add files via upload

---
+++ _next/static/ItqbMcMFG1TjXNdOV_Lpr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/9555570c4df44ce4.css","static/chunks/pages/index-a8941f5cad272955.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
